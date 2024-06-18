0b9e2ba9f3225d32de17711365f38eff100b4e7a
lara <laragonsilva26122003@gmail.com>
2024-06-18 20:56:05 -0300

    exc 5 menu else if

--- src/estruturaCondicional/Condicionais.java
+++ src/estruturaCondicional/Condicionais.java
@@ -57,52 +57,72 @@ public class Condicionais {
 				sc.nextLine();
 				System.out.println("Digite o horario inicial do jogo: ");
 				String horaInicial = sc.nextLine();
-				
+
 				System.out.println("Digite o horario final  do jogo: ");
 				String horaFinal = sc.nextLine();
-				
+
 				DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
-				
+
 				LocalTime timeInicial = null;
 				LocalTime timeFinal = null;
-				
+
 				timeInicial = LocalTime.parse(horaInicial, timeFormat);
 				System.out.println("Hora " + timeInicial);
 				timeFinal = LocalTime.parse(horaFinal, timeFormat);
 				System.out.println("Hora " + timeFinal);
-				
-				
+
 				long duracaoHours = ChronoUnit.HOURS.between(timeInicial, timeFinal);
 				long duracaoMin = ChronoUnit.MINUTES.between(timeInicial, timeFinal);
-				long duracaoHora = duracaoMin /60;
-				long duracaoFinal = duracaoMin %60;
-				
-				//System.out.println("A duração do jogo foi de : " + duracaoHours);
-				//System.out.println("A duração do jogo foi de: " + duracaoFinal );
-				//System.out.println(duracaoMin);
-				
+				long duracaoHora = duracaoMin / 60;
+				long duracaoFinal = duracaoMin % 60;
+
+				// System.out.println("A duração do jogo foi de : " + duracaoHours);
+				// System.out.println("A duração do jogo foi de: " + duracaoFinal );
+				// System.out.println(duracaoMin);
+
 				String duracaoFormatada = String.format("%02d:%02d", duracaoHora, duracaoFinal);
 				System.out.println("A duração foi de " + duracaoFormatada);
-				
-				
+
 				break;
 			case 5:
-				double CQ = 4.00;
-				double xsalada = 4.50;
-				double xbacon = 5.00;
-				double torradaSimples = 2.00;
-				double refrigerante = 1.50;
-				int opcaoComida;
-				do {
-					System.out.println(
-							"Digite as opções que voce escolheu \n 1-Qc\n 2-xsalada \n 3-xbacon \n 4- torrada simples \n 5-refrigerante");
-					opcaoComida = sc.nextInt();
-					System.out.println("Voce escolheu as opções: " + opcaoComida);
+				/*
+				 * String order1 = "Cachorro Quente"; String order2 = "X-Salada"; String order3
+				 * = "X-Bacon"; String order4 = "Torrada Simples"; String order5 = "Refrigente";
+				 */
+				System.out.println("Digite o numero do que deseja pedir: ");
+				int pedido = sc.nextInt();
+				System.out.println("Digite a quantidade: ");
+				int quantidade = sc.nextInt();
 
-				} while (opcaoComida != 0);
+				double CachorroQuente = 4.00;
+				double XSalada = 4.50;
+				double XBacon = 5.00;
+				double TorradaSimples = 2.00;
+				double Refrigente = 1.50;
+
+				double total1 = 0;
+				if (pedido == 1) {
+					total1 = CachorroQuente * quantidade;
+
+				} else if (pedido == 2) {
+					total1 = XSalada * quantidade;
+
+				} else if (pedido == 3) {
+					total1 = XBacon * quantidade;
+
+				} else if (pedido == 4) {
+					total1 = TorradaSimples * quantidade;
+
+				} else if (pedido == 5) {
+					total1 = Refrigente * quantidade;
+
+				}
+
+				System.out.printf("O total a pagar é %.2f", total1);
 			}
 
-			sc.close();
+			
 		} while (opcao != 7);
+		sc.close();
 	}
 }
