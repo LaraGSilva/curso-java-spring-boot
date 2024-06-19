02fc014340905ceed90a169eba3c48e681d9889a
lara <laragonsilva26122003@gmail.com>
2024-06-18 21:00:54 -0300

    exc else if menu

--- src/estruturaCondicional/Condicionais.java
+++ src/estruturaCondicional/Condicionais.java
@@ -16,16 +16,16 @@ public class Condicionais {
 			opcao = sc.nextInt();
 			switch (opcao) {
 			case 1:
-
 				System.out.println("Digite um numero");
 				int number = sc.nextInt();
 
 				if (number >= 0) {
 					System.out.println("Esse numero é positivo");
 				} else if (number < 0) {
-					System.out.println("Esse numero é negatvio");
+					System.out.println("Esse numero é negativo");
 				}
 				break;
+
 			case 2:
 				System.out.println("Digite um numero");
 				int numero = sc.nextInt();
@@ -33,58 +33,52 @@ public class Condicionais {
 				if (numero % 2 == 0) {
 					System.out.println("Esse numero é par");
 				} else {
-					System.out.println("Esse numero é impar");
+					System.out.println("Esse numero é ímpar");
 				}
 				break;
+
 			case 3:
 				System.out.println("Digite o valor de A: ");
 				int a = sc.nextInt();
 				sc.nextLine();
-				System.out.println("Digite o valor deB: ");
+				System.out.println("Digite o valor de B: ");
 				int b = sc.nextInt();
 				sc.nextLine();
 
 				if (a % b == 0) {
-					System.out.println("São multiplos");
+					System.out.println("São múltiplos");
 				} else if (b % a == 0) {
-					System.out.println("São multiplos");
+					System.out.println("São múltiplos");
 				} else {
-					System.out.println("Não são multiplos");
+					System.out.println("Não são múltiplos");
 				}
 				break;
 
 			case 4:
 				sc.nextLine();
-				System.out.println("Digite o horario inicial do jogo: ");
+				System.out.println("Digite o horário inicial do jogo (HH:mm): ");
 				String horaInicial = sc.nextLine();
 
-				System.out.println("Digite o horario final  do jogo: ");
+				System.out.println("Digite o horário final do jogo (HH:mm): ");
 				String horaFinal = sc.nextLine();
 
 				DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
 
-				LocalTime timeInicial = null;
-				LocalTime timeFinal = null;
+				LocalTime timeInicial = LocalTime.parse(horaInicial, timeFormat);
+				System.out.println("Hora inicial: " + timeInicial);
+				LocalTime timeFinal = LocalTime.parse(horaFinal, timeFormat);
+				System.out.println("Hora final: " + timeFinal);
 
-				timeInicial = LocalTime.parse(horaInicial, timeFormat);
-				System.out.println("Hora " + timeInicial);
-				timeFinal = LocalTime.parse(horaFinal, timeFormat);
-				System.out.println("Hora " + timeFinal);
-
-				long duracaoHours = ChronoUnit.HOURS.between(timeInicial, timeFinal);
 				long duracaoMin = ChronoUnit.MINUTES.between(timeInicial, timeFinal);
 				long duracaoHora = duracaoMin / 60;
 				long duracaoFinal = duracaoMin % 60;
 
-				// System.out.println("A duração do jogo foi de : " + duracaoHours);
-				// System.out.println("A duração do jogo foi de: " + duracaoFinal );
-				// System.out.println(duracaoMin);
-
 				String duracaoFormatada = String.format("%02d:%02d", duracaoHora, duracaoFinal);
 				System.out.println("A duração foi de " + duracaoFormatada);
-
 				break;
+
 			case 5:
+
 				/*
 				 * String order1 = "Cachorro Quente"; String order2 = "X-Salada"; String order3
 				 * = "X-Bacon"; String order4 = "Torrada Simples"; String order5 = "Refrigente";
@@ -119,10 +113,30 @@ public class Condicionais {
 				}
 
 				System.out.printf("O total a pagar é %.2f", total1);
-			}
+				break;
 
-			
+			case 6:
+				System.out.println("Digite um valor");
+				double valor = sc.nextDouble();
+
+				if (valor > 0 && valor < 25) {
+					System.out.println("Intervalo (0,25]");
+				} else if (valor > 25 && valor < 50) {
+					System.out.println("Intervalo (25,50]");
+				} else if (valor > 50 && valor < 75) {
+					System.out.println("Intervalo (50, 75]");
+				} else if (valor > 75 && valor < 100) {
+					System.out.println("Intervalo (75,100]");
+				} else {
+					System.out.println("Fora do intervalo");
+				}
+				break;
+
+			case 7:
+				break;
+			}
 		} while (opcao != 7);
+
 		sc.close();
 	}
 }
